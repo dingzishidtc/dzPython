0be81ac3ebfe98378b11f9be80086027dd72f4f6
dingzishidtc <443155612@qq.com>
2020-05-16 16:12:40 +0800

    2020/05/16 16:12:40 增加的测试文件

--- 1.bat
+++
@@ -1,2 +0,0 @@
-rem CODE BY t0nsha 
-echo "%date:~0,10% %time:~0,-3% 增加的测试文件" >>file.txt
--- autogit.bat
+++ autogit.bat
@@ -2,4 +2,4 @@ rem CODE BY t0nsha
 git add *
 git commit -m "%date:~0,10% %time:~0,-3% 增加的测试文件"
 git push
-echo "%date:~0,10% %time:~0,-3% 增加的测试文件" >>gitlog.txt
+echo "%date:~0,10% %time:~0,-3% 变更文件已提交至git远程仓库" >>gitlog.txt
